9e0b68e50a61c81a4bae8b4304518cae237b54df
Bala Naren Chanumolu <4568naren@gmail.com>
2024-09-04 13:20:52 -0400

    Added put, get, post, delete mappings

--- rest-app/src/main/java/com/bn/rest_app/controller/CloudVendorAPIService.java
+++ rest-app/src/main/java/com/bn/rest_app/controller/CloudVendorAPIService.java
@@ -1,17 +1,34 @@
 package com.bn.rest_app.controller;
 
 import com.bn.rest_app.model.CloudVendor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cloudvendor")
 public class CloudVendorAPIService {
 
+    private CloudVendor cloudVendor;
+
     @GetMapping("{vendorId}")
     public CloudVendor getCloudVendorDetails( String vendorId) {
-        return new CloudVendor("C1","C1_Name","C1_Address","C1_Phone");
+        return cloudVendor;
+    }
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor created";
+    }
+
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor updated";
+    }
+
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(String vendorId) {
+        this.cloudVendor = null;
+        return "Cloud Vendor deleted";
     }
 }
